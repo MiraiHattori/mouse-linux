e6d1c4465293c7fb59927c83818f8c5b69f11bba
Mirai Hattori <mirai2045@gmail.com>
2020-07-28 12:26:04 +0900

    update

--- raspi/setup_serial.sh
+++ raspi/setup_serial.sh
@@ -2,3 +2,10 @@
 
 sudo systemctl stop serial-getty@ttyAMA0.service
 sudo systemctl disable serial-getty@ttyAMA0.service
+if [ -z "$(grep "enable_uart=1" /boot/config.txt)" ]; then
+    echo "enable_uart=1" >> /boot/config.txt
+fi
+if [ -z "$(grep "dtoverlay=pi3-miniuart-bt" /boot/config.txt)" ]; then
+    echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+fi
+sudo sed -i 's/console=serial0,115200 //g' /boot/config.txt
