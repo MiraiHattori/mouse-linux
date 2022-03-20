a5c4ff5376f9063571f48dbf725d3943fc3faba0
Mirai Hattori <mirai2045@gmail.com>
2022-03-20 08:55:44 +0000

    add debug print

--- raspi/client/mouse_reader.hpp
+++ raspi/client/mouse_reader.hpp
@@ -42,10 +42,14 @@ public:
       fprintf(stderr, "[MouseReader] can't read mouse event");
       return false;
     }
+    printf("[MouseReader] ");
     if (mouse.type == EV_SYN) {
-      return true;
+      printf("EV_SYN ");
+      printf("\n");
+      return false;
     }
     if (mouse.type == EV_KEY) {
+      printf("EV_KEY ");
       if (mouse.code == BTN_LEFT) {
         if (mouse.value == 0) {
           m_buf[BUTTON] &= (0xff - (0b1 << 0));
@@ -78,6 +82,7 @@ public:
         }
       }
     } else if (mouse.type == EV_REL) {
+      printf("EV_REL ");
       if (mouse.code == REL_WHEEL) {
         m_buf[WHEEL_L] = (mouse.value >> 0) & 0xff;
         m_buf[WHEEL_H] = (mouse.value >> 8) & 0xff;
@@ -89,19 +94,21 @@ public:
         m_buf[Y_H] = (mouse.value >> 8) & 0xff;
       }
     } else if (mouse.type == EV_MSC) {
+      printf("EV_MSC ");
       // left click code 0x90001 / 0d589825
       // right click code 0x90002 / 0d589826
       // middle click code 0x90003 / 0d589827
       // side click code 0x90004 / 0d589828
       // extra click code 0x90005 / 0d589829
       // unused
+    } else {
+      printf("EV_NOTUSED ");
     }
-    printf("[MouseReader] ");
     for (size_t i = 0; i < BUFSIZE; i++) {
       printf("%d ", m_buf[i]);
     }
     printf("\n");
-    return false;
+    return true;
   }
   static void sigint_handler(int /* signo */) {
     printf("\nSIGINT\n");
